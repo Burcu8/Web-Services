8d13c17dc3e1786e16274f4883fc1048455b4821
Burcu8 <burcucelebi33@gmail.com>
2023-12-12 13:37:52 +0300

    Update CRUD operations

--- database/mongodb.py
+++ database/mongodb.py
@@ -4,30 +4,4 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client["film_db"]
 collection = db["contents"]
 
-# the data you want to add
-data_list = [
-    {
-        "title" : "Sample Content 1",
-        "year" : 2023,
-        "length" : 120,
-        "producer" : "Sample Producer 1",
-        "description" : "This is a sample content 1.",
-        "genre" : ["Action", "Adventure"],
-    },
-    {
-        "title": "Sample Content 2",
-        "year": 2023,
-        "length": 90,
-        "producer": "Sample Producer 2",
-        "description": "This is a sample content 2.",
-        "genre": ["Drama", "Romance"],
-    },
-    #Other data you want to add...
-]
-
-# add data to MongoDB
-result = collection.insert_many(data_list)
-
-#print the IDs of the attached documents
-print(f"Inserted documents IDs: {result.inserted_ids}")
 
--- main.py
+++ main.py
@@ -1,46 +1,53 @@
 from fastapi import FastAPI, HTTPException
 from models.models import Content
 from database.mongodb import collection
+from bson import ObjectId
 
 app = FastAPI()
 
-@app.get("/")
 
-def read_rood():
-    return {"message": "Hello, FastAPI!"}
-
-
-#yeni içerik eklemek içim
+#CREATE
 @app.post("/content/")
 def create_content(content: Content):
-    content_id = collection.insert_many(content.model_dump()).inserted_id  #dict() yerine model_dump kullanıldı.
+    content_id = collection.insert_one(content.model_dump()).inserted_id  #dict() yerine model_dump kullanıldı.
     return {"message": "Content created succesfully", "content_id": str(content_id)}
 
 
-#içerikleri listelemek için
-@app.get("/content")
+@app.get("/content/")
 def read_contents():
     contents = list(collection.find())
+    for content in contents:
+        content["_id"] = str(content["_id"])
     return {"contents": contents}
 
 
-#içerik güncellemek için
+#READ
+@app.get("/content/{content_id}")
+def read_contents(content_id: str):
+   # return {"content" : []}
+    contents = list(collection.find({"_id": ObjectId(content_id)}))
+    for content in contents:
+        content["_id"] = str(content["_id"])
+    return {"contents": contents}
+
+
+#UPDATE
 @app.put("/content/{content_id}")
-def update_content(content_id: str, update_content: Content):
+def update_content(content_id: int, update_content: Content):
     result = collection.update_one({"_id": content_id}, {"$set": update_content.model_dump()})
     
     if result.modified_count == 1:
         # Get the details of the updated content when the update is successful
         updated_document = collection.find_one({"_id": content_id})
-        return {"massage": "content updated succesfully", "updated content": updated_document}
+        return {"message": "content updated succesfully", "updated content": updated_document}
 
     else:
         raise HTTPException(status_code=404, detail = "Content not found")
 
 
-#içerik silmek için
+#DELETE
 @app.delete("/content/{content_id}")
-def delete_content(content_id: str):
+def delete_content(content_id: int):
     result = collection.delete_one({"_id": content_id})
     
     if result.deleted_count == 1:
