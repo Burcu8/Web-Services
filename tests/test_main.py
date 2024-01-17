428b531a4b275a501dbb7cd398944fe114ef2ab1
Burcu8 <burcucelebi33@gmail.com>
2024-01-17 17:44:03 +0300

    read details test

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,8 @@ from main import create_app
 from routes.route import router
 from models.models import Content
 from database.mongodb import collection
+from bson import ObjectId
+
 
 app = create_app()
 client = TestClient(app)
@@ -20,5 +22,16 @@ def test_read_contents_success_and_verify_count():
     assert len(response.json()['contents']) == expected_count
 
 
+
+def test_read_details_contents_success_and_verify_count():
+    
+    response = client.get("/content/656efee0b171fd8c704b2b2c")
+    assert response.status_code == 200
+
+    dbclient = MongoClient("mongodb://localhost:27017/")
+    db = dbclient["film_db"]
+    collection = db["contents"]
+    expected_count = collection.count_documents({"_id": ObjectId("656efee0b171fd8c704b2b2c")})
+    assert len(response.json()['contents']) == expected_count
 
 
