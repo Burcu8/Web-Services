f8064e5bc16b028ab5baf4659b3e4f80cdca9aaa
Burcu8 <burcucelebi33@gmail.com>
2024-01-17 20:10:32 +0300

    delete endpoint test

--- tests/test_main.py
+++ tests/test_main.py
@@ -55,4 +55,16 @@ def test_update_content_success_and_verify_count():
     collection = db["contents"]
     expected_count = collection.count_documents({"_id": ObjectId(content_id)})
     assert expected_count == 1
-            
+
+
+def test_delete_content_success_and_verify_count():
+    
+    content_id = "65a7f1603db16dfbbcdfd0a4"
+    response = client.delete(f"/content/{content_id}")
+    assert response.status_code == 200
+        
+    dbclient = MongoClient("mongodb://localhost:27017/")
+    db = dbclient["film_db"]
+    collection = db["contents"]
+    expected_count = collection.count_documents({"_id": ObjectId(content_id)})
+    assert expected_count == 0
