56ed088edd37f843455278e7246a3119489ba2ef
Burcu8 <burcucelebi33@gmail.com>
2024-01-26 15:43:07 +0300

    post test

--- tests/test_main.py
+++ tests/test_main.py
@@ -21,7 +21,25 @@ def test_read_contents_success_and_verify_count():
     expected_count = collection.count_documents({})
     assert len(response.json()['contents']) == expected_count
 
-
+def test_create_content_success():
+    
+    content = {
+        "title": "test content5",
+        "year": 2021,
+        "length": 120,
+        "producer": "test producer",
+        "description": "test description",
+        "genre": "test genre"
+    }
+    
+    response = client.post("/content/", json=content)
+    assert response.status_code == 200
+    
+    dbclient = MongoClient("mongodb://localhost:27017/")
+    db = dbclient["film_db"] 
+    collection = db["contents"]
+    expected_id = collection.find_one(content)["_id"]
+    assert response.json()['content_id'] == str(expected_id)
 
 def test_read_details_contents_success_and_verify_count():
     
@@ -37,7 +55,7 @@ def test_read_details_contents_success_and_verify_count():
 
 def test_update_content_success_and_verify_count():
 
-    content_id = "65a7f1603db16dfbbcdfd0a4"
+    content_id = "65a8e1e81f7764917fccdff8"
     content = {
         "title": "test content2",
         "year": 2021,
@@ -59,7 +77,7 @@ def test_update_content_success_and_verify_count():
 
 def test_delete_content_success_and_verify_count():
     
-    content_id = "65a7f1603db16dfbbcdfd0a4"
+    content_id = "65a8e1e81f7764917fccdff8"
     response = client.delete(f"/content/{content_id}")
     assert response.status_code == 200
         
