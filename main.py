d371fb5ff40d7f1343979f6ffb196ed5bddccd60
Burcu8 <burcucelebi33@gmail.com>
2024-02-13 14:31:40 +0300

    fixed main file and delete mongodb.js

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from fastapi import FastAPI, APIRouter, HTTPException
-from models.models import Content
-from bson import ObjectId
-from pydantic import ConfigDict
-
-
+from fastapi import FastAPI
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 app = FastAPI()
@@ -18,16 +13,6 @@ class Config(BaseSettings):
     model_config = SettingsConfigDict(env_file = '.env')
 
 settings = Config()
-
-def print_settings():
-    from main import settings
-    print(settings.app_name)
-    print(settings.host)
-    print(settings.port)
-    print(settings.mongo_client)
-    print(settings.db_name)
-    print(settings.collection_name)
-print_settings()
 
 def create_app():
     from routes.route import router
